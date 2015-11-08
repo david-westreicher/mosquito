da7cfecc1806aeb129a49bea2c983e1aa34d28a2
baum <baum@oddurl.de>
2015-11-08 03:08:40 +0100

    fps madness!!!

--- desktop/src/com/madness/mosquito/desktop/DesktopLauncher.java
+++ desktop/src/com/madness/mosquito/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.madness.mosquito.MosquitoMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.vSyncEnabled = false;
+		config.backgroundFPS = 0;
+		config.foregroundFPS = 0;
 		new LwjglApplication(new MosquitoMain(), config);
 	}
 }
