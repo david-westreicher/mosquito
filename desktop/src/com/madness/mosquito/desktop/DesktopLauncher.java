d314ccd31b27bd8e1f5c0e062c60481dce8d1925
david <david@localhost.localdomain>
2015-11-08 01:12:31 +0100

    init

---
+++ desktop/src/com/madness/mosquito/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.madness.mosquito.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.madness.mosquito.MosquitoMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MosquitoMain(), config);
+	}
+}
